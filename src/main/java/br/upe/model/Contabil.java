54505e286df3f8c1ffa511233094ab1364ed79bf
Kayo Renato <kayorenatocontato@gmail.com>
2021-12-03 22:17:57 -0300

    Mapeamento de relacao Contabil e Conta

--- src/main/java/br/upe/model/Contabil.java
+++ src/main/java/br/upe/model/Contabil.java
@@ -5,9 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.List;
 
 @Getter 
@@ -19,6 +17,9 @@ public class Contabil implements EntidadeBase{
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private double saldoGlobal;
+
+	@OneToMany
+	@JoinColumn(name = "contabil_id")
 	private List<Conta> contas;
 	
 }
