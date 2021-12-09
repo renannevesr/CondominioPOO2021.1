78ad9a190ff678e8510baa3dc72ce11bc02ebf4f
Will <dsilverfox98@gmail.com>
2021-12-08 21:15:14 -0300

    Primeiro service

--- src/main/java/br/upe/App.java
+++ src/main/java/br/upe/App.java
@@ -1,7 +1,10 @@
 package br.upe;
 
+import java.util.Date;
+
 import br.upe.model.dao.ContaDAO.JPAContaDAO;
 import br.upe.model.dao.RequisicaoProdutoDAO.JPARequisicaoProdutoDAO;
+import br.upe.model.entity.Funcionario;
 import br.upe.model.service.FuncionarioService;
 
 
@@ -12,20 +15,25 @@ public class App {
         //Conta novaConta = new Conta();
         //Movimentacao novaMovimentacao = new Movimentacao(350.0, null, novaConta, "1", null);
              
-        JPAContaDAO contaDao = new JPAContaDAO();
-        JPARequisicaoProdutoDAO requisicaoProdutoDAO = new JPARequisicaoProdutoDAO();
+        //JPAContaDAO contaDao = new JPAContaDAO();
+        //JPARequisicaoProdutoDAO requisicaoProdutoDAO = new JPARequisicaoProdutoDAO();
         //movimentacaoDAO.salvar(novaMovimentacao);
        
-        System.out.println(requisicaoProdutoDAO.listar());
-        System.out.println(contaDao.listar());
-        System.out.println(requisicaoProdutoDAO.buscarPorId(1l).getValorTotal());
+        //System.out.println(requisicaoProdutoDAO.listar());
+        //System.out.println(contaDao.listar());
+        //System.out.println(requisicaoProdutoDAO.buscarPorId(1l).getValorTotal());
         //listar as movimentacoes por conta
-        System.out.println(contaDao.buscarPorId(1l).getMovimentacao());
-        
+        //System.out.println(contaDao.buscarPorId(1l).getMovimentacao());
+        //Funcionario novoFuncionario = new Funcionario(new Date(2020, 10, 20), "201535", "Porteiro");
+
         System.out.println("Chama as meretrizes que a multa tá paga");
         
+        @SuppressWarnings("deprecation")
+        Funcionario novoFuncionario = new Funcionario(new Date(120, 10, 20), "201985", "Jardineiro");
+        novoFuncionario.setNome("Tuca do degrade");
+        novoFuncionario.setCpf("12345686145");
         FuncionarioService funcionario = new FuncionarioService();
-        funcionario.cadastrar(null);
+        funcionario.cadastrar(novoFuncionario);
         
     }
     
--- src/main/java/br/upe/model/service/FuncionarioService.java
+++ src/main/java/br/upe/model/service/FuncionarioService.java
@@ -9,13 +9,17 @@ public class FuncionarioService {
 
 	private JPAFuncionarioDAO dao;
 	
+	public FuncionarioService() {
+		this.dao = new JPAFuncionarioDAO();
+	}
+	
 	public void cadastrar(Funcionario funcionario) {
 		
 		Date data = new Date();
 		
-		if(funcionario.getCpf().length() != 11) {
+		if(funcionario.getCpf().length() != 11 || !funcionario.getCpf().matches("\\d+")) {
 			System.out.println("O campo de CPF está incorreto!");
-		}else if(data.after(funcionario.getDataAdmissao())){
+		}else if(data.before(funcionario.getDataAdmissao())){
 			System.out.println("A data que você inseriu é maior que a atual.");
 		}else{
 			dao.salvar(funcionario);
