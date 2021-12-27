cb0322f523aec6f83515586b45987117ed1ea687
Luis Eduardo Santos Costa <luiseduardo70@live.com>
2021-12-27 18:52:37 -0300

    Inserção dos dados na TableView

--- src/main/java/br/upe/controllersJavaFX/CondominoViewController.java
+++ src/main/java/br/upe/controllersJavaFX/CondominoViewController.java
@@ -13,13 +13,20 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart.Data;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class CondominoViewController implements Initializable{
 	
     @FXML
     private ComboBox<Blocos> bloco_AP;
+    
+    @FXML
+    private ComboBox<Integer> num_AP;
 
     @FXML
     private ComboBox<?> button_funcionario;
@@ -43,13 +50,23 @@ public class CondominoViewController implements Initializable{
     private TextField nome;
 
     @FXML
-    private ComboBox<Integer> num_AP;
+    private TableView<Condomino> condominoTable;
 
     @FXML
-    void Select(ActionEvent event) {
+    private TableColumn<Condomino, String> tableCPF;
 
-    }
+    @FXML
+    private TableColumn<Condomino, String> tableContato;
 
+    @FXML
+    private TableColumn<Condomino, String> tableNome;
+    
+    @FXML
+    private TableColumn<Data, Void> tableAcoes;
+
+    @FXML
+    void Select(ActionEvent event) {}
+   
 	CondominoController condominoController = new CondominoController();
 	
 	@FXML
@@ -57,22 +74,39 @@ public class CondominoViewController implements Initializable{
 		String nome = this.nome.getText();
 		String cpf = this.cpf.getText();
 		String contato = this.contato.getText();
-		int numero = (int) this.num_AP.getSelectionModel().getSelectedItem();
+		
+		int numero = 0;
+		if(this.num_AP.getSelectionModel().getSelectedItem() != null)
+			numero = (int) this.num_AP.getSelectionModel().getSelectedItem(); 
+		
 		Blocos bloco = (Blocos) this.bloco_AP.getSelectionModel().getSelectedItem();
 		
-		Apartamento ap = new Apartamento();
-		ap.setBloco(bloco);
-		ap.setNumero(numero);
-		
 		try {
-			Condomino condomino = new Condomino();
-			condomino.setNome(nome);
-			condomino.setCpf(cpf);
-			condomino.setContato(contato);
-			condominoController.cadastrar(condomino, ap);
-			Alerts.alertSuccess("Condomino cadastrado com sucesso!");
-			
-		}catch(Exception e) {
+			if(nome.equals("") || 
+			   cpf.equals("") || 
+			   contato.equals("") || 
+			   bloco.equals("") ||
+			   numero == 0 ) 
+			{
+				Alerts.alertError("Seu burro, sua anta, preencha tudo sua misera!");
+			}
+			else {
+				Apartamento ap = new Apartamento();
+				ap.setBloco(bloco);
+				ap.setNumero(numero);
+				
+				Condomino condomino = new Condomino();
+				condomino.setNome(nome);
+				condomino.setCpf(cpf);
+				condomino.setContato(contato);
+				condominoController.cadastrar(condomino, ap);
+				
+				Alerts.alertSuccess("Condomino cadastrado com sucesso!");
+				
+				atualizaTabela();
+			}
+		}
+		catch(Exception e) {
 			Alerts.alertError("Erro ao tentar cadastrar esse Condomino!");
 		}
 	}
@@ -87,7 +121,7 @@ public class CondominoViewController implements Initializable{
 	}
 	
 	@FXML
-	private void atualizar(Condomino condomino) {
+	private void atualizarCondomino(Condomino condomino) {
 		try {
 			condominoController.atualizar(condomino);
 		}catch (Exception e) {
@@ -96,13 +130,22 @@ public class CondominoViewController implements Initializable{
 	}
 	
 	@FXML
-	private void excluir(Condomino condomino) {
+	private void excluirCondomino(Condomino condomino) {
 		try {
 			condominoController.remover(condomino);
+			atualizaTabela();
 		}catch(Exception e) {
 			Alerts.alertError("Não foi possível excluir esse condomino!");
 		}
 	}
+	
+	private void atualizaTabela() {
+        try {
+            condominoTable.setItems(FXCollections.observableArrayList(condominoController.listar()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -111,7 +154,18 @@ public class CondominoViewController implements Initializable{
 		
 		num_AP.setItems(list1);
 		bloco_AP.setItems(list2);
-	
+		
+		// Tabela
+		try {
+			ObservableList<Condomino> list3 = FXCollections.observableArrayList(condominoController.listar());
+			
+			tableNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+			tableCPF.setCellValueFactory(new PropertyValueFactory<>("cpf"));
+			tableContato.setCellValueFactory(new PropertyValueFactory<>("contato"));
+			
+			condominoTable.setItems(list3);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-
 }
