920f54e09288cfa3aeac77ebc071810189516753
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-01-04 11:00:41 -0300

    DTO de tabela veiculo

---
+++ src/main/java/br/upe/controllersJavaFX/TableVeiculoAp.java
@@ -0,0 +1,30 @@
+package br.upe.controllersJavaFX;
+
+import br.upe.model.entity.Apartamento;
+import br.upe.model.entity.Blocos;
+import br.upe.model.entity.Veiculo;
+import lombok.Data;
+
+@Data
+public class TableVeiculoAp {
+
+	private Long idAp;
+	private Long idVeiculo;
+	private int numero;
+	private Blocos bloco;
+	private String placa;
+	private String modelo;
+	private String cor;
+
+	
+	public TableVeiculoAp(Veiculo v, Apartamento a) {
+		this.numero = a.getNumero();
+		this.bloco = a.getBloco();
+		this.idAp = a.getId();
+		this.placa = v.getPlaca();
+		this.modelo = v.getPlaca();
+		this.cor = v.getCor();
+		this.idVeiculo = v.getId();
+	}
+	
+}
